b4e2d07f635905eea4ebac32814c8d36c6bfc33f
Syed Huzaifa <68459184+SyedHuzaifa007@users.noreply.github.com>
2025-05-09 12:18:26 +0500

    Update

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const serverless = require('serverless-http');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-module.exports.handler = serverless(app);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
