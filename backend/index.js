66dfe56f8baefa17b4cb2750e45cd1a91273113e
Syed Huzaifa <68459184+SyedHuzaifa007@users.noreply.github.com>
2025-05-10 15:04:22 +0500

    Update urls

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
+require('dotenv').config()
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
-mongoose.connect(mongoose.connect(process.env.MONGODB_URI), {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('MongoDB connection error:', err);
 });
 
 app.listen(5000, () => console.log('Server running on port 5000'));
